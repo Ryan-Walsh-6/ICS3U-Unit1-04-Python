fc6de69bc00f5929e94b4c045e656bbfdca68551
Ubuntu <ubuntu@ip-172-31-84-227.ec2.internal>
2020-11-24 00:26:40 +0000

    add changes

---
+++ math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# created by: Ryan Walsh
+# created on: November 2020
+# this program solves for area and perimeter
+
+import math
+
+
+def main():
+    # this program solves for area and perimeter
+
+    print("If the rectangle has dimensions of :")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {} cm^2.".format(5*3))
+    print("Perimeter is {} cm.".format(2*(5+3)))
+
+
+if __name__ == "__main__":
+    main()
